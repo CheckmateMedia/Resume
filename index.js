9b2e5b401ecbade81651af1517c8f40d55df6f44
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-07-27 18:54:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function goodbye() {
     document.getElementById("hidden").style.display = "block";
     console.log("working")
 }
+
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
